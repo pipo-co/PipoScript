4b2c4d3b1b14e3459607f96c57d3538d9c803a25
Ignacio <isagues@itba.edu.ar>
2020-11-26 18:12:13 -0300

    Little change in errors/run.sh

--- errors/run.sh
+++ errors/run.sh
@@ -3,15 +3,15 @@
 index=0
 
 FILES=./*.pipo
-
+error=0
 for f in $FILES
 do
     
     $COMPILER $f 2> /dev/null > /dev/null
-    
-    if [ $? != 0 ]
+    error=$?
+    if [ "$error" != 0 ]
     then
-        echo "File #"$index":" "Error Code: " $?" "$f "cause an error as expected"
+        echo "File #"$index":" "Error Code: " $error" "$f "cause an error as expected"
     else
         echo "\nAn error was expected :C. \nFile" $f "\n"
         exit 1
